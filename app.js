cb173c1416ceae23775e4ca019b58d55b97410a3
annruiblaz <anna.ruiz.dev@gmail.com>
2025-02-18 18:03:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const { text } = require('stream/consumers');
+
+const filePath = 'archivo.txt';
+
+//Crea o abre (si ya exisita) un archivo en modo a+ (lectura / escritura)
+/* fs.open('archivo.txt', 'a+', (err, fd) => {
+    if( err ) {
+        console.error('No se puede crear o abrir el fichero.', err);
+    } else {
+        console.log('Se ha creado el archivo o ya existia');
+        fs.close(fd, () => {}); //Cerramos el archivo
+    }
+}); */
+
+//Verifica si el archivo existe antes de abrirlo
+fs.access(filePath, fs.constants.F_OK, (err) => {
+    const existiaAntes = !err; //Si no hay error es xq el archivo ya existia
+
+    //Crea o abre (si ya exisita) un archivo en modo a+ (lectura / escritura)
+    fs.open('archivo.txt', 'a+', (err, fd) => {
+        if( err ) {
+            console.error('No se puede crear o abrir el fichero.', err);
+        } else {
+            console.log(existiaAntes ? 'El archivo ya existia' : 'Se ha creado el archivo');
+            fs.close(fd, () => {}); //Cierra el archivo
+        }
+    });
+});
+
+//Escribe el txt en el archivo (es asincrono)
+// **sobrescribiendolo si ya existe el archivo
+const texto = 'La materia s transforma ...';
+fs.writeFile('texto1.txt', texto, (err) => {
+    if(err) {
+        console.log('Error al escribir en el archivo:', err);
+    } else {
+        console.log('Texto almacenado correctamente');
+    }
+});
+
+//Escribir el txt en el archivo pero d manera sincrona
+// ** Tambien sobrescribiendolo si ya existe el archivo
+try {
+    fs.writeFileSync('textoSincrono.txt', texto);
+    console.log('Texto almacenado con exito.');
+} catch(e) {
+    console.error(e);
+}
+
+
+//Igual pero separando el proceso de abrir + escribir
+//** Al utilizar 'w' machaca el contenido anterior del fichero
+fs.open('archivo2.txt', 'w', (err, fd) => {
+    if( err ) throw err;
+
+    fs.write(fd, 'Contenido nuevo', (err) => {
+        if (err) throw err;
+        console.log('Archivo escrito con éxito.');
+
+        fs.close(fd, (err) => {
+            if (err) throw err;
+            console.log('Archivo cerrado.');
+        });
+    });
+});
+
+//Para leer el txt d un archivo
+fs.readFile('texto1.txt', 'utf8', (err, data) => {
+    if(err) {
+        console.error('Error al leer el archivo: ', err);
+    } else {
+        console.log('Contenido del archivo: ', data);
+    }
+});
